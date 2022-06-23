f91baeee89cfbf6c7661d86dab74bc7e3f869878
JinuCheon <woojin8787@gmail.com>
2022-06-23 15:34:09 +0900

    init: spring boot initialization

---
+++ src/main/java/com/dope/breaking/BreakingApplication.java
@@ -0,0 +1,13 @@
+package com.dope.breaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dope/breaking/BreakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dope.breaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

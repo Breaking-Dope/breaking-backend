5e639bc5938b27eaf99b619a57618933c38c152c
최현영 <choihyunyeong@choehyeon-yeong-ui-MacBookPro.local>
2022-06-29 16:31:59 +0900

    feat: added UserDetails for Spring Security Authentication

---
+++ src/main/java/com/dope/breaking/Security/UserDetails/PrincipalDetails.java
@@ -0,0 +1,55 @@
+package com.dope.breaking.Security.UserDetails;
+
+import com.dope.breaking.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(() -> user.getRoleKey());
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

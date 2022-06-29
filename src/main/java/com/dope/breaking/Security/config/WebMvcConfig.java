40fc275be60425af0439dcf729d81cf47a668233
최현영 <choihyunyeong@choehyeon-yeong-ui-MacBookPro.local>
2022-06-29 16:39:35 +0900

    feat: added Cors Configuration for Front-End

---
+++ src/main/java/com/dope/breaking/Security/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.dope.breaking.Security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) { //CORS 정책 추가.
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}

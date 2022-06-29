da8c2e1add2ad3fd5df91206a3b2bfce69788b0d
최현영 <choihyunyeong@choehyeon-yeong-ui-MacBookPro.local>
2022-06-29 16:21:24 +0900

    feat: added PasswordEncoder and set Spring Bean

--- src/main/java/com/dope/breaking/BreakingApplication.java
+++ src/main/java/com/dope/breaking/BreakingApplication.java
@@ -2,9 +2,16 @@ package com.dope.breaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BreakingApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BreakingApplication.class, args);

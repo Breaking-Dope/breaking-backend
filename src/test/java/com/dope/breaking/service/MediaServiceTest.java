a3338c6ad5ba82fd50d07ba62f7dc9e398403c46
MinwuKim <minwu1221@gmail.com>
2022-06-30 21:31:13 +0900

    test: add test case for "createMediaEntities" method

--- src/test/java/com/dope/breaking/service/MediaServiceTest.java
+++ src/test/java/com/dope/breaking/service/MediaServiceTest.java
@@ -1,16 +1,26 @@
 package com.dope.breaking.service;
 
 import com.dope.breaking.domain.media.MediaType;
+import com.dope.breaking.domain.post.Post;
+import com.dope.breaking.repository.PostRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @SpringBootTest
+@Transactional
+@Rollback(false)
 class MediaServiceTest {
 
     @Autowired MediaService mediaService;
+    @Autowired PostRepository postRepository;
 
     @Test
     void findMediaType() {
@@ -24,6 +34,14 @@ class MediaServiceTest {
         Assertions.assertThat(mediaType1).isEqualTo(MediaType.PHOTO);
         Assertions.assertThat(mediaType2).isEqualTo(MediaType.VIDEO);
 
+    }
+
+    @Test
+    void createMediaEntities(){
+        List<String> fileNameList = Arrays.asList("1.png","2.mov","3.jpeg");
+        Post post = new Post();
+        postRepository.save(post);
+        mediaService.createMediaEntities(fileNameList,post);
     }
 
 }

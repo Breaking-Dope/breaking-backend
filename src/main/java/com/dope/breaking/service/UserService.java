ab74bf8d5c22fd20e98f9ec1abda47ec21104476
MinwuKim <minwu1221@gmail.com>
2022-06-30 14:41:26 +0900

    chore: fix "findby" to "findBy"

--- src/main/java/com/dope/breaking/service/UserService.java
+++ src/main/java/com/dope/breaking/service/UserService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,7 +33,10 @@ public class UserService {
         return err;
     }
 
-    public Optional<User> findbyUsername(String username) {
+    public Optional<User> findByUsername(String username) {
+
         return userRepository.findByUsername(username);
+
     }
+    
 }

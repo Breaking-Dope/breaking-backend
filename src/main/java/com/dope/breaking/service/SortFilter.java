9dd1024c13051f9730e3e054e2a27907df03b3ec
JinuCheon <woojin8787@gmail.com>
2022-07-04 11:00:12 +0900

    feat: validation sort filter value by enum

--- src/main/java/com/dope/breaking/service/SortFilter.java
+++ src/main/java/com/dope/breaking/service/SortFilter.java
@@ -4,4 +4,12 @@ public enum SortFilter {
     LIKE,
     VIEW,
     CHRONOLOGICAL;
+
+    public static SortFilter findMatchedEnum(String str) {
+        for (SortFilter el : SortFilter.values()) {
+            if (el.name().equalsIgnoreCase(str))
+                return el;
+        }
+        return null;
+    }
 }

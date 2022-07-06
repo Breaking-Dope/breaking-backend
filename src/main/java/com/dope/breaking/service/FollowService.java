865986e046ab7c2784778ddfa7238e8bdaa60834
MinwuKim <minwu1221@gmail.com>
2022-07-06 17:26:57 +0900

    feat: implement FollowRepository and FollowService

---
+++ src/main/java/com/dope/breaking/service/FollowService.java
@@ -0,0 +1,12 @@
+package com.dope.breaking.service;
+
+import com.dope.breaking.repository.FollowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+
+    private final FollowRepository followRepository;
+}

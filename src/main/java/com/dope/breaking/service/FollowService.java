187646d53591b4269a087728fc7d6bd9e9e3fcd8
MinwuKim <minwu1221@gmail.com>
2022-07-07 22:11:20 +0900

    refactor: relocated method to FollowService from controller

--- src/main/java/com/dope/breaking/service/FollowService.java
+++ src/main/java/com/dope/breaking/service/FollowService.java
@@ -2,11 +2,13 @@ package com.dope.breaking.service;
 
 
 import com.dope.breaking.domain.user.Follow;
+import com.dope.breaking.domain.user.User;
 import com.dope.breaking.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -21,5 +23,43 @@ public class FollowService {
     }
 
     public Optional<Follow> findById(Long followId) {return followRepository.findById(followId);}
+
+    public Boolean isFollowing (User followingUser, User followedUser) {
+
+        List<Follow> followingList = followingUser.getFollowingList();
+
+        for (Follow follow : followingList) {
+            if (follow.getFollowed() == followedUser) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void AFollowB (User followingUser, User followedUser){
+
+        Follow follow = new Follow();
+
+        follow.updateFollowing(followingUser);
+        follow.updateFollowed(followedUser);
+
+        followingUser.getFollowingList().add(follow);
+        followedUser.getFollowerList().add(follow);
+
+    }
+
+    public void AUnfollowB (User followingUser, User followedUser){
+
+        for (Follow follow : followingUser.getFollowingList()) {
+
+            if (follow.getFollowed() == followedUser){
+
+                followingUser.getFollowingList().remove(follow);
+                followedUser.getFollowerList().remove(follow);
+                break;
+
+            }
+        }
+    }
 
 }

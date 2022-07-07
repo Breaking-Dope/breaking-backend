e3e9f26c3f8c6fb333e36a52469106c6a831a56d
MinwuKim <minwu1221@gmail.com>
2022-07-07 22:11:46 +0900

    test: implemented test cases for FollowService

--- src/test/java/com/dope/breaking/service/FollowServiceTest.java
+++ src/test/java/com/dope/breaking/service/FollowServiceTest.java
@@ -1,12 +1,15 @@
 package com.dope.breaking.service;
 
-import com.dope.breaking.domain.user.Follow;
 import com.dope.breaking.domain.user.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
@@ -16,20 +19,54 @@ class FollowServiceTest {
 
     @Autowired private UserService userService;
 
+    @Autowired private EntityManager em;
+
+
     @Test
-    public void followCheck(){
+    void isFollowing() {
 
         //Given
         User followingUser = new User();
         User followedUser = new User();
 
         //When
-        Follow follow = new Follow();
-        followingUser.addFollowing(follow, followedUser);
+        followService.AFollowB(followingUser,followedUser);
 
         //Then
-        Assertions.assertThat(followingUser.getFollowingList().size()).isEqualTo(1);
+        assertTrue(followService.isFollowing(followingUser,followedUser));
 
     }
 
+    @Test
+    void AFollowB() {
+
+        //Given
+        User followingUser = new User();
+        User followedUser = new User();
+
+        //When
+        followService.AFollowB(followingUser,followedUser);
+
+        //Then
+        Assertions.assertThat(followingUser.getFollowingList().get(0).getFollowed()).isEqualTo(followedUser);
+        Assertions.assertThat(followedUser.getFollowerList().get(0).getFollowing()).isEqualTo(followingUser);
+
+
+    }
+
+    @Test
+    void AUnfollowB() {
+
+        //Given
+        User followingUser = new User();
+        User followedUser = new User();
+
+        //When
+        followService.AFollowB(followingUser,followedUser);
+        followService.AUnfollowB(followingUser,followedUser);
+
+        //Then
+        Assertions.assertThat(followingUser.getFollowingList().size()).isEqualTo(0);
+        Assertions.assertThat(followedUser.getFollowerList().size()).isEqualTo(0);
+    }
 }

20f248b621ca7636af8eac9dedee28aebe2ea202
JinuCheon <woojin8787@gmail.com>
2022-07-08 11:16:01 +0900

    docs: fix PostCreateException message and comment

--- src/main/java/com/dope/breaking/exception/post/PostCreateException.java
+++ src/main/java/com/dope/breaking/exception/post/PostCreateException.java
@@ -3,11 +3,14 @@ package com.dope.breaking.exception.post;
 import com.dope.breaking.exception.BreakingException;
 import org.springframework.http.HttpStatus;
 
+/**
+ * oauth2.0 에서 발급된 username이 데이터베이스에 없을 때 발생하는 예외입니다.
+ */
 public class PostCreateException extends BreakingException {
 
     private static final String MESSAGE = "유저 인증에 실패하였습니다." ;
 
     public PostCreateException() {
-        super(MESSAGE, HttpStatus.UNAUTHORIZED); //적절한 에러 코드 사용 강조
+        super(MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

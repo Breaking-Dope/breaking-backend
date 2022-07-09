60c2cbe1ec44e09e0dad6159e715d13a69cfa7e9
MinwuKim <minwu1221@gmail.com>
2022-07-09 20:56:35 +0900

    feat: implement follower/following list

--- src/main/java/com/dope/breaking/service/FollowService.java
+++ src/main/java/com/dope/breaking/service/FollowService.java
@@ -3,13 +3,24 @@ package com.dope.breaking.service;
 
 import com.dope.breaking.domain.user.Follow;
 import com.dope.breaking.domain.user.User;
+import com.dope.breaking.dto.response.MessageResponseDto;
+import com.dope.breaking.dto.user.FollowInfoResponseDto;
+import com.dope.breaking.exception.oauth.InvalidAccessTokenException;
+import com.dope.breaking.exception.user.NoSuchUserException;
 import com.dope.breaking.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @Transactional
 @Service
@@ -17,6 +28,7 @@ import java.util.Optional;
 public class FollowService {
 
     private final FollowRepository followRepository;
+    private final UserService userService;
 
     public void deleteById(Long followId){
         followRepository.deleteById(followId);
@@ -62,4 +74,35 @@ public class FollowService {
         }
     }
 
+    public ResponseEntity<?> followingUsers (Long userId){
+
+        User user = userService.findById(userId).orElseThrow(NoSuchUserException::new);
+
+        List<Follow> followingList = user.getFollowingList();
+        List<FollowInfoResponseDto> followInfoResponseDtoList = new ArrayList<>();
+
+        for (Follow follow : followingList) {
+            User followedUser = follow.getFollowed();
+            followInfoResponseDtoList.add (new FollowInfoResponseDto(followedUser.getId(),followedUser.getNickname(),followedUser.getStatusMsg(),followedUser.getProfileImgURL()));
+        }
+
+        return ResponseEntity.ok().body(followInfoResponseDtoList);
+    }
+
+    public ResponseEntity<?> followerUsers (Long userId){
+
+
+        User user = userService.findById(userId).orElseThrow(NoSuchUserException::new);
+
+
+        List<Follow> followerList = user.getFollowerList();
+        List<FollowInfoResponseDto> followInfoResponseDtoList = new ArrayList<>();
+
+        for (Follow follow : followerList) {
+            User followedUser = follow.getFollowing ();
+            followInfoResponseDtoList.add (new FollowInfoResponseDto(followedUser.getId(),followedUser.getNickname(),followedUser.getStatusMsg(),followedUser.getProfileImgURL()));
+        }
+
+        return ResponseEntity.ok().body(followInfoResponseDtoList);
+    }
 }

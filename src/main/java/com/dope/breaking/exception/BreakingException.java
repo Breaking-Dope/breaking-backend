a2318ec878698ffa204b28cb6e8a5f59da3cca49
JinuCheon <woojin8787@gmail.com>
2022-07-14 01:54:24 +0900

    refactor: define error codes

--- src/main/java/com/dope/breaking/exception/BreakingException.java
+++ src/main/java/com/dope/breaking/exception/BreakingException.java
@@ -4,15 +4,21 @@ import org.springframework.http.HttpStatus;
 
 public class BreakingException extends RuntimeException {
 
+    private ErrorCode errorCode;
     private final HttpStatus status;
 
-    public BreakingException(final String message, final HttpStatus status) {
-        super(message);
+    public BreakingException(final ErrorCode errorCode, final HttpStatus status) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
         this.status = status;
     }
 
     public HttpStatus getStatus() {
         return status;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
     }
 
 }
--- src/main/java/com/dope/breaking/exception/auth/InvalidAccessTokenException.java
+++ src/main/java/com/dope/breaking/exception/auth/InvalidAccessTokenException.java
@@ -4,14 +4,14 @@ import com.dope.breaking.exception.BreakingException;
 import org.springframework.http.HttpStatus;
 
 /**
- * oauth2.0 에서 발급된 username이 데이터베이스에 없을 때 발생하는 예외입니다.
+ * Access Token이 올바르지 않을 때 발생하는 예외입니다.
+ * 1. oauth2.0 에서 발급된 username이 데이터베이스에 없을 때.
+ * 2. Access Token이 디코딩이 안될때.
  */
 public class InvalidAccessTokenException extends BreakingException {
 
-    private static final String MESSAGE = "유저 인증에 실패하였습니다." ;
-
     public InvalidAccessTokenException() {
-        super(MESSAGE, HttpStatus.UNAUTHORIZED);
+        super(ErrorCode.INVALID_ACCESS_TOKEN, HttpStatus.UNAUTHORIZED);
     }
 
 }

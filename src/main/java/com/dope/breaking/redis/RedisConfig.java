ec9bfab1b03d5ccf4f957cc769018ffd6706c8ea
Martin0o0 <8546088@naver.com>
2022-07-14 15:41:38 +0900

    feat: build a service class to use redis

--- src/main/java/com/dope/breaking/redis/RedisConfig.java
+++ src/main/java/com/dope/breaking/redis/RedisConfig.java
@@ -1,2 +1,42 @@
-package com.dope.breaking.redis;public class RedisConfig {
+package com.dope.breaking.redis;
+
+import com.querydsl.core.annotations.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        //일반적인 key:value의 경우 시리얼라이저
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+
+        return redisTemplate;
+    }
+
+
+
 }

3cf522dbddf00c9dd67eba2481e9d8e7a49c08a3
MinwuKim <minwu1221@gmail.com>
2022-07-21 15:23:07 +0900

    feat: add NoSuchCommentException

---
+++ src/main/java/com/dope/breaking/exception/comment/NoSuchCommentException.java
@@ -0,0 +1,11 @@
+package com.dope.breaking.exception.comment;
+
+import com.dope.breaking.exception.BreakingException;
+import com.dope.breaking.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public class NoSuchCommentException extends BreakingException {
+
+    public NoSuchCommentException() { super (ErrorCode.NO_SUCH_COMMENT, HttpStatus.BAD_REQUEST);}
+
+}

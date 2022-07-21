479c0a2c0b87af81399e0ae19ef9572f1d689e3e
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-07-21 15:21:53 +0900

    fix: initialize static media folder (#117)

--- src/main/java/com/dope/breaking/BreakingApplication.java
+++ src/main/java/com/dope/breaking/BreakingApplication.java
@@ -4,19 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.CacheControl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaAuditing //JPA auditing 설정
+import java.util.concurrent.TimeUnit;
+
+@EnableJpaAuditing
 @SpringBootApplication
-public class BreakingApplication {
+public class BreakingApplication implements WebMvcConfigurer {
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/")
+				.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BreakingApplication.class, args);
 	}
+
+
 
 }

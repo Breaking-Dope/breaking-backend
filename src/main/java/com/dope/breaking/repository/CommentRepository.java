200df478c3a774ab392621cfa5b2aa0e69389f59
MinwuKim <minwu1221@gmail.com>
2022-07-22 13:15:49 +0900

    feat: update comment service

--- src/main/java/com/dope/breaking/repository/CommentRepository.java
+++ src/main/java/com/dope/breaking/repository/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.dope.breaking.repository;
 
 import com.dope.breaking.domain.comment.Comment;
+import com.dope.breaking.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+
 }

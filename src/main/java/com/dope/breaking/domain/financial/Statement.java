c50f4514d61c1d814036a20095e796b1b42df9dc
MinwuKim <minwu1221@gmail.com>
2022-07-28 20:30:34 +0900

    feat: update Purchase and Statement entities

--- src/main/java/com/dope/breaking/domain/financial/Statement.java
+++ src/main/java/com/dope/breaking/domain/financial/Statement.java
@@ -1,12 +1,15 @@
 package com.dope.breaking.domain.financial;
 
 import com.dope.breaking.domain.user.User;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Statement {
 
     @Id @GeneratedValue
@@ -17,6 +20,18 @@ public class Statement {
     @JoinColumn (name = "USER_ID")
     private User user;
 
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
     private int amount;
+
+    @Builder
+    public Statement(User user, TransactionType transactionType, int amount){
+
+        this.user = user;
+        this.transactionType = transactionType;
+        this.amount = amount;
+
+    }
 
 }

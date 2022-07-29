6327e34ad4385a95c830991d9b35d96504cb4e2b
MinwuKim <minwu1221@gmail.com>
2022-07-29 21:19:48 +0900

    feat: add PurchaseService and TransactionService

--- src/main/java/com/dope/breaking/service/TransactionService.java
+++ src/main/java/com/dope/breaking/service/TransactionService.java
@@ -1,7 +1,9 @@
 package com.dope.breaking.service;
 
+import com.dope.breaking.domain.financial.Purchase;
 import com.dope.breaking.domain.financial.Statement;
 import com.dope.breaking.domain.financial.Transaction;
+import com.dope.breaking.domain.financial.TransactionType;
 import com.dope.breaking.domain.user.User;
 import com.dope.breaking.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,4 +23,15 @@ public class TransactionService {
         transactionRepository.save(transaction);
 
     }
+
+    public void purchasePostTransaction (User buyer, User seller, Purchase purchase){
+
+        Transaction buyerTransaction = new Transaction(buyer, null, purchase, purchase.getPrice(), TransactionType.BUY_POST);
+        Transaction sellerTransaction = new Transaction(seller, null, purchase, purchase.getPrice(), TransactionType.SELL_POST);
+        transactionRepository.save(buyerTransaction);
+        transactionRepository.save(sellerTransaction);
+
+    }
+
+
 }

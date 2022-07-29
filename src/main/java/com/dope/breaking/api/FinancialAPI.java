2aa3c30d30ecd578b4db26e6e953aef91816e97b
MinwuKim <minwu1221@gmail.com>
2022-07-29 21:23:02 +0900

    feat: add controller for purchasing post in FinancialAPI

--- src/main/java/com/dope/breaking/api/FinancialAPI.java
+++ src/main/java/com/dope/breaking/api/FinancialAPI.java
@@ -2,10 +2,12 @@ package com.dope.breaking.api;
 
 import com.dope.breaking.domain.financial.TransactionType;
 import com.dope.breaking.dto.financial.AmountRequestDto;
+import com.dope.breaking.service.PurchaseService;
 import com.dope.breaking.service.StatementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,11 @@ import java.security.Principal;
 public class FinancialAPI {
 
     private final StatementService statementService;
+    private final PurchaseService purchaseService;
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/financial/deposit")
-    public ResponseEntity deposit(Principal principal,@RequestBody @Valid AmountRequestDto depositAmount) {
+    public ResponseEntity deposit (Principal principal,@RequestBody @Valid AmountRequestDto depositAmount) {
 
         statementService.depositOrWithdraw(principal.getName(), depositAmount.getAmount(), TransactionType.DEPOSIT);
         return ResponseEntity.ok().build();
@@ -30,9 +33,18 @@ public class FinancialAPI {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/financial/withdraw")
-    public ResponseEntity withdraw(Principal principal, @RequestBody @Valid AmountRequestDto withdrawAmount) {
+    public ResponseEntity withdraw (Principal principal, @RequestBody @Valid AmountRequestDto withdrawAmount) {
 
         statementService.depositOrWithdraw(principal.getName(), withdrawAmount.getAmount(), TransactionType.WITHDRAW);
+        return ResponseEntity.ok().build();
+
+    }
+
+    @PreAuthorize("isAuthenticated")
+    @PostMapping("/post/{postId}/purchase")
+    public ResponseEntity purchase (Principal principal, @PathVariable Long postId) {
+
+        purchaseService.purchasePost(principal.getName(), postId);
         return ResponseEntity.ok().build();
 
     }

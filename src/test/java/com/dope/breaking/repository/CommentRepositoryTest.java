1a933fee98b9d30ba72bf2b6e161feee3e944c40
JinuCheon <woojin8787@gmail.com>
2022-07-29 22:34:25 +0900

    test: make to return true when i liked comment

--- src/test/java/com/dope/breaking/repository/CommentRepositoryTest.java
+++ src/test/java/com/dope/breaking/repository/CommentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.dope.breaking.repository;
 
 import com.dope.breaking.domain.comment.Comment;
+import com.dope.breaking.domain.comment.CommentLike;
 import com.dope.breaking.domain.post.Post;
 import com.dope.breaking.domain.user.User;
 import com.dope.breaking.dto.comment.CommentResponseDto;
@@ -24,9 +25,10 @@ class CommentRepositoryTest {
     @Autowired CommentRepository commentRepository;
     @Autowired UserRepository userRepository;
     @Autowired PostRepository postRepository;
+    @Autowired CommentLikeRepository commentLikeRepository;
     @Autowired EntityManager em;
 
-    @DisplayName("댓글은 커서 페이지네이션이 적용된다.")
+    @DisplayName("댓글에는 커서 페이지네이션이 적용된다.")
     @Test
     void commentPagination() {
 
@@ -59,5 +61,36 @@ class CommentRepositoryTest {
         assertEquals(4, content1.size());
         assertEquals(4, content2.size());
         assertEquals(2, content3.size(), ()->"마지막 페지이는 2개가 나온다.");
+    }
+
+    @DisplayName("내가 좋아요한 게시글은, isLiked 필드가 true로 반환된다.")
+    @Test
+    void CheckIsLikeFieldFromCommentList() {
+
+        User me = new User();
+        userRepository.save(me);
+
+        Post post = new Post();
+        postRepository.save(post);
+
+        Comment comment = new Comment(me, post, "댓글");
+        commentRepository.save(comment);
+
+        CommentLike commentLike = new CommentLike(me, comment);
+        commentLikeRepository.save(commentLike);
+
+        em.flush();
+        em.clear();
+
+        SearchCommentConditionDto searchCommentConditionDto = SearchCommentConditionDto.builder()
+                .targetId(post.getId())
+                .targetType(CommentTargetType.POST)
+                .size(1L)
+                .cursorId(null)
+                .build();
+
+        List<CommentResponseDto> content = commentRepository.searchCommentList(me, searchCommentConditionDto);
+
+        assertTrue(content.get(0).getIsLiked());
     }
 }

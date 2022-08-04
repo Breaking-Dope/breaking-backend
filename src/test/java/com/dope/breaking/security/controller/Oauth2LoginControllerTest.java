769775b8585afeafbe915a872f6d283503204a35
Martin0o0 <8546088@naver.com>
2022-08-04 14:58:28 +0900

    teat: test fot already login

--- src/test/java/com/dope/breaking/security/controller/Oauth2LoginControllerTest.java
+++ src/test/java/com/dope/breaking/security/controller/Oauth2LoginControllerTest.java
@@ -1,6 +1,9 @@
 package com.dope.breaking.security.controller;
 
+import com.dope.breaking.domain.user.User;
+import com.dope.breaking.repository.UserRepository;
 import com.dope.breaking.security.jwt.JwtTokenProvider;
+import com.dope.breaking.service.RedisService;
 import com.dope.breaking.service.UserService;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,6 +20,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +29,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
@@ -37,6 +43,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class Oauth2LoginControllerTest {
 
     @Autowired
@@ -46,17 +53,24 @@ class Oauth2LoginControllerTest {
     private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private RedisService redisService;
 
 
-    @DisplayName("구글 Oauth2 로그인")
+    private final String accesstoken = "";
+    private final String idtoken = "";
+
+    private final String USERNAME = "";
+
+    @DisplayName("유효한 엑스스토큰으로 구글 Oauth2 로그인을 시도 시, 정상적으로 유저 정보가 반환된다.")
     @Test
     void googleOauthLogin() throws Exception {
-        String accesstoken = ""; String idtoken = "";
+
         Map<String, String> info = new LinkedHashMap<>();
 
         info.put("accessToken", accesstoken);
@@ -65,14 +79,12 @@ class Oauth2LoginControllerTest {
         String content = objectMapper.writeValueAsString(info);
         System.out.println(content);
         MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/oauth2/sign-in/google")
-                .content(content)
-                .contentType(MediaType.APPLICATION_JSON))
+                        .post("/oauth2/sign-in/google")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.fullname").value("최현영[학생](소프트웨어융합대학 컴퓨터공학부)"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value(""))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(""))
                 .andReturn();
 
         MockHttpServletResponse response = result.getResponse();
@@ -80,9 +92,50 @@ class Oauth2LoginControllerTest {
         JsonNode jsonNode = objectMapper.readTree(rescontent);
         ObjectNode objectNode = (ObjectNode) jsonNode;
         System.out.println(objectNode.toPrettyString());
+    }
+
+    @DisplayName("이미 가입한 유저가 유효한 엑스스토큰으로 구글 Oauth2 로그인을 시도 시, 예외를 반환한다.")
+    @Test
+    void googleOauthLoginWithSignUpUser() throws Exception {
+        User user = User.builder()
+                .username(USERNAME).build();
+
+        userRepository.save(user);
+
+        Map<String, String> info = new LinkedHashMap<>();
+        info.put("accessToken", accesstoken);
+        info.put("idToken", idtoken);
+        String content = objectMapper.writeValueAsString(info);
+        System.out.println(content);
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders
+                        .post("/oauth2/sign-in/google")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+
+        MockHttpServletResponse response = result.getResponse();
+        System.out.println("accessToken : " + response.getHeaderValue("Authorization"));
+        String accessjwt = (String) response.getHeaderValue("Authorization");
+        System.out.println("refreshToken : " + response.getHeaderValue("Authorization-refresh"));
+        String refreshjwt = (String) response.getHeaderValue("Authorization-refresh");
+
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .post("/oauth2/sign-in/google").header("Authorization","Bearer " + accessjwt)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        redisService.deleteValues("104431269882060562135g");
 
 
     }
+
+
 
 
 }

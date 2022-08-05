f0eb5a6cd3a55ee9e0ddb4f6eb3541885b526116
MinwuKim <minwu1221@gmail.com>
2022-08-05 14:35:10 +0900

    test: update test case to confirm

--- src/main/java/com/dope/breaking/repository/PostRepository.java
+++ src/main/java/com/dope/breaking/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.dope.breaking.repository;
 
 import com.dope.breaking.domain.post.Post;
+import com.dope.breaking.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
+
     Boolean existsByIdAndUserId(Long postid, Long userid);
 
+    int countPostByUser(User user);
 
 }

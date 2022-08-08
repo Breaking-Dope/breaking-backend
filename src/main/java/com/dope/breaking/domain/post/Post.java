d9d65760b09563382a53cd0b166a443972e4b886
MinwuKim <minwu1221@gmail.com>
2022-08-08 14:27:01 +0900

    feat: extend varchar limit for content in database

--- src/main/java/com/dope/breaking/domain/post/Post.java
+++ src/main/java/com/dope/breaking/domain/post/Post.java
@@ -1,7 +1,5 @@
 package com.dope.breaking.domain.post;
 
-
-
 import com.dope.breaking.domain.baseTimeEntity.BaseTimeEntity;
 import com.dope.breaking.domain.comment.Comment;
 import com.dope.breaking.domain.financial.Purchase;
@@ -59,6 +57,7 @@ public class Post extends BaseTimeEntity {
 
     private String title;
 
+    @Column(length = 2500)
     private String content;
 
     @Enumerated(EnumType.STRING)

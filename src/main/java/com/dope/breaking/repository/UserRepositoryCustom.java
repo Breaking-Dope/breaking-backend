0182f08ca5e494641719bbb9bffc350cf976a291
JinuCheon <woojin8787@gmail.com>
2022-08-15 10:38:22 +0900

    feat: setup querydsl enviroment

---
+++ src/main/java/com/dope/breaking/repository/UserRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.dope.breaking.repository;
+
+public interface UserRepositoryCustom {
+}
---
+++ src/main/java/com/dope/breaking/repository/UserRepositoryCustomImpl.java
@@ -0,0 +1,4 @@
+package com.dope.breaking.repository;
+
+public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+}

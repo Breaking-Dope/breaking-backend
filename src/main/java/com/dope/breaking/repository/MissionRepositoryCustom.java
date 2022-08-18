157ac574f0d2461b9faf8a9ef92b6f33ef8878d6
JinuCheon <woojin8787@gmail.com>
2022-08-18 14:02:54 +0900

    feat: implement searching mission search query

---
+++ src/main/java/com/dope/breaking/repository/MissionRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.dope.breaking.repository;
+
+import com.dope.breaking.domain.post.Mission;
+import com.dope.breaking.domain.user.User;
+import com.dope.breaking.dto.mission.MissionFeedResponseDto;
+
+import java.util.List;
+
+public interface MissionRepositoryCustom {
+
+    List<MissionFeedResponseDto> searchMissionFeed(User me, Mission cursorMission, int size);
+
+}

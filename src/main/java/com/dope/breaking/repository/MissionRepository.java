55492f4aa1b22116d4486de233611fbce93aaa5c
JinuCheon <woojin8787@gmail.com>
2022-08-18 14:02:54 +0900

    feat: implement searching mission search query

--- src/main/java/com/dope/breaking/repository/MissionRepository.java
+++ src/main/java/com/dope/breaking/repository/MissionRepository.java
@@ -3,5 +3,5 @@ package com.dope.breaking.repository;
 import com.dope.breaking.domain.post.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<Mission, Long> {
+public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
 }

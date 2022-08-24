8dd97fb547823e33d2e977a7cfb4ee2d5662f7cf
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-08-24 22:25:03 +0900

    fix: handle RequestRejectedException to response 400 error (#278)
    * fix: handle RequestRejectedException to response 400 error
    
    * refactor: change error response to Slf4j

--- src/main/java/com/dope/breaking/api/CustomExceptionHandler.java
+++ src/main/java/com/dope/breaking/api/CustomExceptionHandler.java
@@ -8,6 +8,7 @@ import com.dope.breaking.exception.NotValidRequestBodyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,6 +19,14 @@ import java.io.StringWriter;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    @ExceptionHandler(RequestRejectedException.class)
+    protected ResponseEntity<ErrorResponseDto> handleRequestRejectedException(RequestRejectedException e) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto(ErrorCode.BAD_REQUEST));
+    }
+
     @ExceptionHandler(CustomInternalErrorException.class)
     protected ResponseEntity<ErrorResponseDto> handleCustomInternalErrorException(CustomInternalErrorException e) {
 
@@ -25,14 +34,13 @@ public class CustomExceptionHandler {
         e.printStackTrace(new PrintWriter(sw));
         String stacktraceAsString = sw.toString();
 
+        log.error("e.getMessage() = " + e.getMessage());
         log.error(stacktraceAsString);
-        System.out.println("e.getMessage() = " + e.getMessage());
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponseDto(ErrorCode.INTERNAL_SERVER_ERROR));
     }
-
 
     @ExceptionHandler(BreakingException.class)
     protected ResponseEntity<ErrorResponseDto> handleBreakingCustomException(BreakingException e) {
@@ -43,12 +51,10 @@ public class CustomExceptionHandler {
                 .body(new ErrorResponseDto(e.getErrorCode()));
     }
 
-
     @ExceptionHandler(NotValidRequestBodyException.class)
     protected  ResponseEntity<ErrorResponseDto> handleNotValidRequestBody(NotValidRequestBodyException e){
         log.info(e.getMessage());
         return ResponseEntity.status(e.getStatus()).body(new ErrorResponseDto(e.getErrorCode(), e.getMessage()));
     }
-
 
 }

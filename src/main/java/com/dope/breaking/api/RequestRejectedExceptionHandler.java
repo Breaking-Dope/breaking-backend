648faef8003dc23e6932c78d9597dedb802e1d1c
JinuCheon <woojin8787@gmail.com>
2022-08-27 22:45:29 +0900

    fix: rename to RequestRejectedExceptionHandler
    from RequestRejectedExceptionController

--- src/main/java/com/dope/breaking/api/RequestRejectedExceptionHandler.java
+++ src/main/java/com/dope/breaking/api/RequestRejectedExceptionHandler.java
@@ -1,9 +1,8 @@
-package com.dope.breaking.security.config;
+package com.dope.breaking.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
@@ -12,14 +11,13 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class RequestRejectedExceptionController extends GenericFilterBean {
+public class RequestRejectedExceptionHandler extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {

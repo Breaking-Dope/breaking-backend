42a74aa35005f4bb08fff98998491b60ce741e2c
JinuCheon(깻묵) <woojin8787@gmail.com>
2022-08-27 22:51:11 +0900

    fix: add filter to handle RequestRejectedException (#287)
    * feat: filter for handle RequestRejectedException
    
    * fix: remove custom excpetion handler
    
    remove RequestRejectedException from custom excpetion
    
    * refactor: remove tasks in this branch
    
    * feat: add filter for RequestRejectedException
    
    * fix: rename to RequestRejectedExceptionHandler
    
    from RequestRejectedExceptionController

---
+++ src/main/java/com/dope/breaking/api/RequestRejectedExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.dope.breaking.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.web.firewall.RequestRejectedException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestRejectedExceptionHandler extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(req, res);
+        } catch (RequestRejectedException e) {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
